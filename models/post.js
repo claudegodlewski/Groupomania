891135d83e3dc1cb1093ddd651d66e9815a40268
Claude Godlewski <96684710+claudegodlewski@users.noreply.github.com>
2022-07-16 17:57:54 +0200

    Refacto + systemUser

--- models/post.js
+++ models/post.js
@@ -1,16 +1,12 @@
-// Importation.
 const mongoose = require('mongoose');
 
-// Modèle.
 const postSchema = mongoose.Schema({
+  systemUser: { type: String, required: true },
   userId: { type: String, required: true },
-  userTitle: { type: String, required: true },
-  UserMessage: { type: String, required: true },
-  //imageUrl: { type: String, required: false },
-  likes: { type: Number, default: 0, required: true },
-  dislikes: { type: Number, default: 0, required: true },
-  usersLiked: { type: Array, default: [], required: true },
-  usersDisliked: { type: Array, default: [], required: true }
+  text: { type: String, trim: true, maxlenght: 500 },
+  imageUrl: { type: String, required: true },
+  likes: { type: Number, required: true },
+  usersLiked: { type: [String], required: true }
 });
 
-module.exports = mongoose.model('post', postSchema);
+module.exports = mongoose.model('Post', postSchema);
